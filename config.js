61a6bd9fa3e254216d5fd15b367b6dfd829b900d
Anthony <30807672+Caccavella@users.noreply.github.com>
2024-03-06 13:05:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,3 @@
 module.exports = {
-  clientId: '869656362729-2ikbgfuvtdp96f08usqb28kv68mnluik.apps.googleusercontent.com',
-  sessionSecret: "Nev43dasl30kdsahenLJHHddd333",
-  cookieName: 'session',
-  stripeSecret: 'sk_test_51HS4trHwvd8YbrSENmcHD7RP7SvU1mbDsjO3Ln2WBEukBta724SYZN9iX1PCRw04czrl1yQVUBX7gS9fHBSkpR4o00LdAHDjt7',
-  stripePub: 'pk_test_51HS4trHwvd8YbrSEWMCxr1Xj2PRM3nnhDqlSa2NrSs5iLZZiy2BKjnmOhCwrYipq0XSsYXu1dWUjECAUnI80IIKH00Jzz6oMWk',
-  // stripeSecret: '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',
-  // stripePub: 'pk_live_51HS4trHwvd8YbrSE5yTx9iHtjmui7ntFY4hHZJTXvXEg9FBNGl9ljb3XOsH5j3CK0PzVJP6So0mbdcLS95867dx000bgeVZeLD',
-  secret: '7YJHTkLpHUgHa6YsKU6IY_8k',
-  saltRounds: 10,
-  // sendApi: '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',
-  sendApi: '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',
-  backendUrl: 'http://localhost:5050',
-  baseUrl: 'http://localhost:5050',
-  frontEndUrl: 'http://localhost:8080',
-  // backendUrl: 'https://personabilities-staging-dot-model-axe-273818.wm.r.appspot.com',
-  // baseUrl: 'https://personabilities-staging-dot-model-axe-273818.wm.r.appspot.com',
-  // frontEndUrl: 'https://personabilities-staging-dot-model-axe-273818.wm.r.appspot.com',
-  // backendUrl: 'https://app.personabilities.com',
-  // baseUrl: 'https://app.personabilities.com',
-  // frontEndUrl: 'https://app.personabilities.com',
-  bucketName: "us.artifacts.model-axe-273818.appspot.com"
+
 }
